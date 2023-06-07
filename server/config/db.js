90553cdc5bc8b3abcf3fce8ff5feb4560f211144
Tayyab Tariq <tayyabashraf22@gmail.com>
2023-06-07 08:22:36 +0500

    Database Encapsulation

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        console.log('testing');
         process.exit(1);
     }
 }
